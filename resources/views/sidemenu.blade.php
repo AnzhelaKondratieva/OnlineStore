7ece70fea976449ee9a569e10dcc41d9229c85e0
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-05-23 13:20:09 +0300

    header anf footer charging

---
+++ resources/views/sidemenu.blade.php
@@ -0,0 +1,411 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Flipmart premium HTML5 & CSS3 Template</title>
+
+	    <!-- Bootstrap Core CSS -->
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    
+	    <!-- Customizable CSS -->
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/blue.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		
+
+		
+		<!-- Icons/Glyphs -->
+		<link rel="stylesheet" href="assets/css/font-awesome.css">
+
+        <!-- Fonts --> 
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+
+	</head>
+    <body class="cnt-home">
+		<!-- ================================== TOP NAVIGATION ================================== -->
+		<div class="side-menu animate-dropdown outer-bottom-xs">
+			<div class="head"><i class="icon fa fa-align-justify fa-fw"></i> Categories</div>
+			<nav class="yamm megamenu-horizontal" role="navigation">
+				<ul class="nav">
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-shopping-bag" aria-hidden="true"></i>Clothing</a>
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Shoes </a></li>
+											<li><a href="#">Jackets</a></li>
+											<li><a href="#">Sunglasses</a></li>
+											<li><a href="#">Sport Wear</a></li>
+											<li><a href="#">Blazers</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shorts</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Handbags</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Swimwear </a></li>
+											<li><a href="#">Tops</a></li>
+											<li><a href="#">Flats</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">Winter Wear</a></li>
+											<li><a href="#">Night Suits</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Toys &amp; Games</a></li>
+											<li><a href="#">Jeans</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">School Bags</a></li>
+											<li><a href="#">Lunch Box</a></li>
+											<li><a href="#">Footwear</a></li>
+											<li><a href="#">Wipes</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Sandals </a></li>
+											<li><a href="#">Shorts</a></li>
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Bags</a></li>
+											<li><a href="#">Night Dress</a></li>
+											<li><a href="#">Swim Wear</a></li>
+											<li><a href="#">Toys</a></li>
+
+										</ul>
+									</div><!-- /.col -->
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-laptop" aria-hidden="true"></i>Electronics</a>
+						<!-- ================================== MEGAMENU VERTICAL ================================== -->
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Gaming</a></li>
+											<li><a href="#">Laptop Skins</a></li>
+											<li><a href="#">Apple</a></li>
+											<li><a href="#">Dell</a></li>
+											<li><a href="#">Lenovo</a></li>
+											<li><a href="#">Microsoft</a></li>
+											<li><a href="#">Asus</a></li>
+											<li><a href="#">Adapters</a></li>
+											<li><a href="#">Batteries</a></li>
+											<li><a href="#">Cooling Pads</a></li>
+										</ul>
+									</div>
+
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Routers &amp; Modems</a></li>
+											<li><a href="#">CPUs, Processors</a></li>
+											<li><a href="#">PC Gaming Store</a></li>
+											<li><a href="#">Graphics Cards</a></li>
+											<li><a href="#">Components</a></li>
+											<li><a href="#">Webcam</a></li>
+											<li><a href="#">Memory (RAM)</a></li>
+											<li><a href="#">Motherboards</a></li>
+											<li><a href="#">Keyboards</a></li>
+											<li><a href="#">Headphones</a></li>
+										</ul>
+									</div>
+
+									<div class="dropdown-banner-holder">
+										<a href="#"><img alt="" src="assets/images/banners/banner-side.png" /></a>
+									</div>
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->
+						<!-- ================================== MEGAMENU VERTICAL ================================== -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-paw" aria-hidden="true"></i>Shoes</a>
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Shoes </a></li>
+											<li><a href="#">Jackets</a></li>
+											<li><a href="#">Sunglasses</a></li>
+											<li><a href="#">Sport Wear</a></li>
+											<li><a href="#">Blazers</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shorts</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Handbags</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Swimwear </a></li>
+											<li><a href="#">Tops</a></li>
+											<li><a href="#">Flats</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">Winter Wear</a></li>
+											<li><a href="#">Night Suits</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Toys &amp; Games</a></li>
+											<li><a href="#">Jeans</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">School Bags</a></li>
+											<li><a href="#">Lunch Box</a></li>
+											<li><a href="#">Footwear</a></li>
+											<li><a href="#">Wipes</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Sandals </a></li>
+											<li><a href="#">Shorts</a></li>
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Bags</a></li>
+											<li><a href="#">Night Dress</a></li>
+											<li><a href="#">Swim Wear</a></li>
+											<li><a href="#">Toys</a></li>
+
+										</ul>
+									</div><!-- /.col -->
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-clock-o"></i>Watches</a>
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Gaming</a></li>
+											<li><a href="#">Laptop Skins</a></li>
+											<li><a href="#">Apple</a></li>
+											<li><a href="#">Dell</a></li>
+											<li><a href="#">Lenovo</a></li>
+											<li><a href="#">Microsoft</a></li>
+											<li><a href="#">Asus</a></li>
+											<li><a href="#">Adapters</a></li>
+											<li><a href="#">Batteries</a></li>
+											<li><a href="#">Cooling Pads</a></li>
+										</ul>
+									</div>
+
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Routers &amp; Modems</a></li>
+											<li><a href="#">CPUs, Processors</a></li>
+											<li><a href="#">PC Gaming Store</a></li>
+											<li><a href="#">Graphics Cards</a></li>
+											<li><a href="#">Components</a></li>
+											<li><a href="#">Webcam</a></li>
+											<li><a href="#">Memory (RAM)</a></li>
+											<li><a href="#">Motherboards</a></li>
+											<li><a href="#">Keyboards</a></li>
+											<li><a href="#">Headphones</a></li>
+										</ul>
+									</div>
+
+									<div class="dropdown-banner-holder">
+										<a href="#"><img alt="" src="assets/images/banners/banner-side.png" /></a>
+									</div>
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-diamond"></i>Jewellery</a>
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Shoes </a></li>
+											<li><a href="#">Jackets</a></li>
+											<li><a href="#">Sunglasses</a></li>
+											<li><a href="#">Sport Wear</a></li>
+											<li><a href="#">Blazers</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shorts</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Handbags</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Swimwear </a></li>
+											<li><a href="#">Tops</a></li>
+											<li><a href="#">Flats</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">Winter Wear</a></li>
+											<li><a href="#">Night Suits</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Toys &amp; Games</a></li>
+											<li><a href="#">Jeans</a></li>
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Shoes</a></li>
+											<li><a href="#">School Bags</a></li>
+											<li><a href="#">Lunch Box</a></li>
+											<li><a href="#">Footwear</a></li>
+											<li><a href="#">Wipes</a></li>
+										</ul>
+									</div><!-- /.col -->
+									<div class="col-sm-12 col-md-3">
+										<ul class="links list-unstyled">
+											<li><a href="#">Sandals </a></li>
+											<li><a href="#">Shorts</a></li>
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Jwellery</a></li>
+											<li><a href="#">Bags</a></li>
+											<li><a href="#">Night Dress</a></li>
+											<li><a href="#">Swim Wear</a></li>
+											<li><a href="#">Toys</a></li>
+
+										</ul>
+									</div><!-- /.col -->
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-heartbeat"></i>Health and Beauty</a>
+						<ul class="dropdown-menu mega-menu">
+							<li class="yamm-content">
+								<div class="row">
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Gaming</a></li>
+											<li><a href="#">Laptop Skins</a></li>
+											<li><a href="#">Apple</a></li>
+											<li><a href="#">Dell</a></li>
+											<li><a href="#">Lenovo</a></li>
+											<li><a href="#">Microsoft</a></li>
+											<li><a href="#">Asus</a></li>
+											<li><a href="#">Adapters</a></li>
+											<li><a href="#">Batteries</a></li>
+											<li><a href="#">Cooling Pads</a></li>
+										</ul>
+									</div>
+
+									<div class="col-xs-12 col-sm-12 col-lg-4">
+										<ul>
+											<li><a href="#">Routers &amp; Modems</a></li>
+											<li><a href="#">CPUs, Processors</a></li>
+											<li><a href="#">PC Gaming Store</a></li>
+											<li><a href="#">Graphics Cards</a></li>
+											<li><a href="#">Components</a></li>
+											<li><a href="#">Webcam</a></li>
+											<li><a href="#">Memory (RAM)</a></li>
+											<li><a href="#">Motherboards</a></li>
+											<li><a href="#">Keyboards</a></li>
+											<li><a href="#">Headphones</a></li>
+										</ul>
+									</div>
+
+									<div class="dropdown-banner-holder">
+										<a href="#"><img alt="" src="assets/images/banners/banner-side.png" /></a>
+									</div>
+								</div><!-- /.row -->
+							</li><!-- /.yamm-content -->
+						</ul><!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-paper-plane"></i>Kids and Babies</a>
+						<!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-futbol-o"></i>Sports</a>
+						<!-- ================================== MEGAMENU VERTICAL ================================== -->
+						<!-- /.dropdown-menu -->
+						<!-- ================================== MEGAMENU VERTICAL ================================== -->            </li><!-- /.menu-item -->
+
+					<li class="dropdown menu-item">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa fa-envira"></i>Home and Garden</a>
+						<!-- /.dropdown-menu -->            </li><!-- /.menu-item -->
+
+				</ul><!-- /.nav -->
+			</nav><!-- /.megamenu-horizontal -->
+		</div><!-- /.side-menu -->
+		<!-- ================================== TOP NAVIGATION : END ================================== -->
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	
+	<!-- For demo purposes – can be removed on production : End -->
+
+	<!-- JavaScripts placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	<!-- For demo purposes – can be removed on production : End -->
+
+	
+
+</body>
+</html>
