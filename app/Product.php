dcc72f1deba416d78ed5a0fe1a974fd7e4cceafa
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-05-23 19:56:48 +0300

    ddd

--- app/Product.php
+++ app/Product.php
@@ -5,40 +5,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
-
-
-class Product {
+class Product extends Authenticatable{
     public static $categories = ['Clothing'=> ['Men' => ['Suits', 'Shoes', 'Jeans', 'Sunglasses', 'Sport Wear', 'Underwear', 'Shirts'],
                                               'Women' => ['Handbags', 'Jewelry', 'Swimwear', 'Underwear', 'Skirts', 'Shoes', 'Winter wear'],
                                               'Boys' => ['Toys & Games', 'Jeans', 'Shirts', 'Shoes', 'Shool bags', 'Suits', 'Winter wear'],
