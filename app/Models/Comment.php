36b2b4ac07e1c62158ffbf7fe7a0a4e2bf2b509a
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-08-05 15:12:01 +0300

    comments-users relation added

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Models\Article;
 
 class Comment extends Model
 {
@@ -15,5 +16,21 @@ class Comment extends Model
 
     public function article() {
         return $this->belongsTo('App\Models\Article', 'article_id');
+    }
+
+
+    public function user() {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+
+    public function child() {
+        return $this->hasMany('App\Models\Comment','parent_id');
+    }
+
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Comment','parent_id');
     }
 }
